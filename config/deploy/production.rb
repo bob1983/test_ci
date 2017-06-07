aa9c5bb9a25596281c60881fdd77d9806b14429b
bob1983 <bob.1983.g@gmail.com>
2017-06-07 22:43:26 +0900

    fixed

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
+set :linked_files, fetch(:linked_files, []).push('.env.production')
 server "ec2-52-197-115-194.ap-northeast-1.compute.amazonaws.com", user: "ec2-user", roles: %w{app db web}
 set :ssh_options, keys: "~/.ssh/bob-key_pair-TOKYO.pem"
